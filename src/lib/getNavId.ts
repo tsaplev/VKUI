b18ddd5d145732d61fde282ee5737795deef3e42
Evgeniya <e.polozova@corp.vk.com>
2021-11-16 15:52:58 +0300

    getNavId: translate warning

--- src/lib/getNavId.ts
+++ src/lib/getNavId.ts
@@ -7,7 +7,7 @@ export interface NavIdProps {
 export function getNavId(props: NavIdProps, warn?: (text: string) => any) {
   const id = props.nav || props.id;
   if (process.env.NODE_ENV === 'development' && !id && warn) {
-    warn('Navigation item should have "nav" or "id" prop');
+    warn('[getNavId] У навигационного элемента должно быть или свойство "nav", или свойство "id"');
   }
   return id;
 }
