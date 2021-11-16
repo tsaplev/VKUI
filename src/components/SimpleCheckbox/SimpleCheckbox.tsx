388a327f6477669490dbbe13025bcb92fbc53ab3
Evgeniya <e.polozova@corp.vk.com>
2021-11-16 15:57:40 +0300

    SimpleCheckbox: translate warnings

--- src/components/SimpleCheckbox/SimpleCheckbox.tsx
+++ src/components/SimpleCheckbox/SimpleCheckbox.tsx
@@ -3,20 +3,14 @@ import Tappable, { ACTIVE_EFFECT_DELAY } from '../Tappable/Tappable';
 import { getClassName } from '../../helpers/getClassName';
 import { classNames } from '../../lib/classNames';
 import { IOS, VKCOM } from '../../lib/platform';
-
 import { Icon20CheckBoxOn, Icon20CheckBoxOff, Icon24CheckBoxOn, Icon24CheckBoxOff, Icon20CheckBoxIndetermanate } from '@vkontakte/icons';
-
 import { HasRef, HasRootRef } from '../../types';
 import { usePlatform } from '../../hooks/usePlatform';
 import { useAdaptivity } from '../../hooks/useAdaptivity';
 import { useExternRef } from '../../hooks/useExternRef';
 import { SizeType } from '../../hoc/withAdaptivity';
 import { warnOnce } from '../../lib/warnOnce';
-
 import './SimpleCheckbox.css';
-
-const warn = warnOnce('SimpleCheckbox');
-const IS_DEV = process.env.NODE_ENV === 'development';
 
 export interface SimpleCheckboxProps extends
   React.InputHTMLAttributes<HTMLInputElement>,
@@ -26,6 +20,7 @@ export interface SimpleCheckboxProps extends
   defaultIndeterminate?: boolean;
 }
 
+const warn = warnOnce('SimpleCheckbox');
 export const SimpleCheckbox: React.FC<SimpleCheckboxProps> = (props: SimpleCheckboxProps) => {
   const { className, style, getRootRef, getRef, indeterminate, defaultIndeterminate, onChange, ...restProps } = props;
   const { sizeY } = useAdaptivity();
@@ -50,13 +45,13 @@ export const SimpleCheckbox: React.FC<SimpleCheckboxProps> = (props: SimpleCheck
     onChange && onChange(event);
   }, [onChange, indeterminate, restProps.checked]);
 
-  if (IS_DEV) {
+  if (process.env.NODE_ENV === 'development') {
     if (defaultIndeterminate && restProps.defaultChecked) {
-      warn('defaultIndeterminate and defaultChecked cannot be true at the same time');
+      warn('"defaultIndeterminate" и "defaultChecked" не могут быть true одновременно');
     }
 
     if (indeterminate && restProps.checked) {
-      warn('indeterminate and checked cannot be true at the same time');
+      warn('"indeterminate" and "checked" не могут быть true одновременно');
     }
   }
 
