6768bfa0c6d2b56148cfc56b07e23066ece02489
Evgeniya <e.polozova@corp.vk.com>
2021-11-18 15:08:59 +0300

    RadioGroup: add RadioGroup component

---
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { FormItem, FormItemProps } from "../FormItem/FormItem";
+import { getClassName } from "../../helpers/getClassName";
+import { usePlatform } from "../../hooks/usePlatform";
+import { classNames } from "../../lib/classNames";
+import "./RadioGroup.css";
+
+export interface RadioGroupProps extends FormItemProps {
+  mode?: "vertical" | "horizontal";
+}
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({
+  mode = "vertical",
+  children,
+  ...restProps
+}: RadioGroupProps) => {
+  const platform = usePlatform();
+
+  return (
+    <FormItem
+      vkuiClass={classNames(
+        getClassName("RadioGroup", platform),
+        `RadioGroup--${mode}`
+      )}
+      Component="fieldset"
+      {...restProps}
+    >
+      <div vkuiClass="RadioGroup__in">{children}</div>
+    </FormItem>
+  );
+};
