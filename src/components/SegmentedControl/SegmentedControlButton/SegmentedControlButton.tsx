76629181b0c0b8d6d9ae1e667b85aceec9fc3380
Evgeniya <e.polozova@corp.vk.com>
2021-12-10 15:07:54 +0300

    SegmentedControlButton: add basic styles

---
+++ src/components/SegmentedControl/SegmentedControlButton/SegmentedControlButton.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Tappable from "../../Tappable/Tappable";
+import { getClassName } from "../../../helpers/getClassName";
+import { classNames } from "../../../lib/classNames";
+import { HasRootRef } from "../../../types";
+import { usePlatform } from "../../../hooks/usePlatform";
+import Text from "../../Typography/Text/Text";
+import "./SegmentedControlButton.css";
+
+export interface SegmentedControlButtonProps
+  extends React.ButtonHTMLAttributes<HTMLElement>,
+    HasRootRef<HTMLElement> {
+  active?: boolean;
+}
+
+export const SegmentedControlButton: React.FC<SegmentedControlButtonProps> = ({
+  active,
+  children,
+  getRootRef,
+  ...restProps
+}) => {
+  const platform = usePlatform();
+  const [focus, setFocus] = React.useState(false);
+
+  const toggleFocus = () => {
+    setFocus(!focus);
+  };
+
+  return (
+    <Tappable
+      {...restProps}
+      vkuiClass={classNames(getClassName("SegmentedControlButton", platform), {
+        ["SegmentedControlButton--active"]: active,
+        ["SegmentedControlButton--focus"]: focus,
+      })}
+      Component="button"
+      getRootRef={getRootRef}
+      aria-pressed={active}
+      onFocus={toggleFocus}
+      onBlur={toggleFocus}
+      hasActive={false}
+      hoverMode="opacity"
+    >
+      <Text weight="medium">{children}</Text>
+    </Tappable>
+  );
+};
